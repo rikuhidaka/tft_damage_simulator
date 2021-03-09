58377bc1326c72e37b751c7c15bf7b67e3d50afa
rikuhidaka <s16161@tokyo.kosen-ac.jp>
2021-03-10 01:53:14 +0900

    クラス作成用のファイル作成

---
+++ champion_class/champion.py
@@ -0,0 +1,6 @@
+class champion:
+    def __init__(self, attack_damage, attack_speed, armor, magical_registance):
+        self.attack_damage = attack_damage
+        self.attack_speed = attack_speed
+        self.armor = armor
+        self.magical_registance = magical_registance
